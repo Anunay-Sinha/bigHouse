b8f6c189e27db0db2cfccf21188a132f26863b0d
anunay <anunay@sinha.in>
2020-07-20 00:35:36 +0530

    Add support for access logs

--- bh-java/accessLogs/src/main/java/com/deeaae/bh/al/application/ALApplication.java
+++ bh-java/accessLogs/src/main/java/com/deeaae/bh/al/application/ALApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-@ComponentScan("com.deeaae.bh.")
+@ComponentScan({"com.deeaae.bh.*","org.commonsources.correlation.*"})
 public class ALApplication {
   public static void main(String[] args) {
     SpringApplication.run(ALApplication.class, args);
--- bh-java/accessLogs/src/main/java/com/deeaae/bh/al/controller/PingController.java
+++ bh-java/accessLogs/src/main/java/com/deeaae/bh/al/controller/PingController.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ping")
 public class PingController {
   @RequestMapping(value = "", method = RequestMethod.GET)
-  public ResponseEntity<Pong> ping(@RequestParam(required = false, defaultValue = "001") String id) {
-    Pong pong = new Pong(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), id, null, null, null);
+  public ResponseEntity<Pong> ping(@RequestParam(required = false, defaultValue = "001") String id, @RequestHeader Map<String, String> headers) {
+    Pong pong = new Pong(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), id, null, null, headers);
     return ResponseEntity.ok(pong);
   }
 
   @RequestMapping(value = "", method = RequestMethod.POST)
-  public ResponseEntity<Pong> postPing( @RequestBody Map<String, String> data) {
-    Pong pong = new Pong(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), null, null, data, null);
+  public ResponseEntity<Pong> postPing( @RequestBody Map<String, String> data, @RequestHeader Map<String, String> headers) {
+    Pong pong = new Pong(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), null, null, data, headers);
     return ResponseEntity.ok(pong);
 
   }
